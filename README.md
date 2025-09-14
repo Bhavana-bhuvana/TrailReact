70e1c530c39c62664531cc696dbb290a38f0bf89
bhavana-bhuvana <bhuvanashivakumar1404@gmail.com>
2025-09-14 11:51:06 +0530

    updaeted springboot

--- README.md
+++ README.md
@@ -10,3 +10,28 @@ Currently, two official plugins are available:
 ## Expanding the ESLint configuration
 
 If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+for css tailswind install for install refer readme .md of ResTailVR
+-----------------------------------------------------------------------------------
+yup → for schema validation
+react-hook-form → form state management (lightweight, best for React)
+@hookform/resolvers → glue code so react-hook-form can use Yup (or Zod, Joi, etc.)
+-----------------------------------------------------------------------------------
+for  donate page validation downloads
+D:\BHqSPIDERS\BHAVANA\React\NavBar1>npm install yup
+
+added 5 packages, and audited 286 packages in 1s
+
+67 packages are looking for funding
+run `npm fund` for details
+
+found 0 vulnerabilities
+
+D:\BHqSPIDERS\BHAVANA\React\NavBar1>
+D:\BHqSPIDERS\BHAVANA\React\NavBar1>npm install react-hook-form @hookform/resolvers
+
+added 3 packages, and audited 289 packages in 7s
+
+68 packages are looking for funding
+run `npm fund` for details
+
+found 0 vulnerabilities
--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,87 @@
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route,useNavigate } from "react-router-dom";
+// import { useEffect } from "react";
+// import About from './Components/About';
+// import Contact from './Components/Contact';
+// import Donate from './Components/Donate';
+// import Home from './Components/Home';
+// import NavBar from './Components/NavBar';
+// import Projects from './Components/Projects';
+// import './index.css';
+// import Footer from "./Components/Footer";
+
+// function App() {
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     const handleShortcut = (e) => {
+//       if (e.ctrlKey && e.shiftKey && e.key === "A") {
+//         navigate("/admin"); // navigate to admin login
+//       }
+//     };
+
+//     window.addEventListener("keydown", handleShortcut);
+//     return () => window.removeEventListener("keydown", handleShortcut);
+//   }, [navigate]);
+//   return (
+//     <div className="app">
+//       <NavBar />
+//       <div className="page">
+//          <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/about" element={<About />} />
+//           <Route path="/contact" element={<Contact />} />
+//           <Route path="/donate" element={<Donate />} />
+//           <Route path="/projects" element={<Projects />} />
+//           <Route path="/admin" element={<AdminLogin />} />
+//         </Routes>
+//       </div>
+
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import About from './Components/About';
 import Contact from './Components/Contact';
 import Donate from './Components/Donate';
 import Home from './Components/Home';
 import NavBar from './Components/NavBar';
 import Projects from './Components/Projects';
-import './index.css';
+import AdminLogin from './Components/AdminLogin';
 import Footer from "./Components/Footer";
+import './index.css';
+import AdminDashBoard from "./Components/AdminDashBoard";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleShortcut = (e) => {
+      if (e.ctrlKey && e.shiftKey && e.key === "N") {
+        navigate("/admin"); // navigate to admin login
+      }
+    };
+    window.addEventListener("keydown", handleShortcut);
+    return () => window.removeEventListener("keydown", handleShortcut);
+  }, [navigate]);
+
   return (
     <div className="app">
       <NavBar />
       <div className="page">
-         <Routes>
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/donate" element={<Donate />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/admin" element={<AdminLogin />} />
+          <Route path="/admin/dashboard" element={<AdminDashBoard />} /> 
         </Routes>
       </div>
-
       <Footer />
     </div>
   );
